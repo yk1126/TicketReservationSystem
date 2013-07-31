e0878aca05a8df35780e5332e48a67a47bb1b9cc
Yedhu Krishnan V P <yedhu@outlook.com>
2013-07-31 17:50:17 +0530

    Finish search without delay page

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ActiveRecord::Base
   validates :travel_from, presence: true
   validates :travel_to, presence: true
-  VALID_TRAVEL_DATE_REGEX = /\d\d\d\d\-\d\d\-\d\d/i
-  validates :travel_date, presence: true, format: { with: VALID_TRAVEL_DATE_REGEX } 
+  VALID_TRAVEL_DATE_REGEX = /\d\d\d\d\-\d\d-\d\d/i
+  validates :travel_date, presence: true, format: { with: VALID_TRAVEL_DATE_REGEX }
+  
 end
